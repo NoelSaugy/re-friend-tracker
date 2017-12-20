e0cd318f1548ab1dac261f77f86d08119c68313d
NoelSaugy <34703884+NoelSaugy@users.noreply.github.com>
2017-12-20 02:41:35 -0500

    Update guimodel.ts

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -9,7 +9,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "Noëls Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
